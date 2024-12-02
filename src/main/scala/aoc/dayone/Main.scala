ca3beb0dc8b6b387416a9a8f31ab79adf43400b3
Tessa Hoad <contact@tessahoad.co.uk>
2024-12-02 20:31:55 +0000

    Day One

---
+++ src/main/scala/aoc/dayone/Main.scala
@@ -0,0 +1,36 @@
+package aoc.dayone
+
+import scala.io.Source
+
+@main def partOne(args: String*): Unit =
+  val source = Source.fromResource("dayone/input.txt")
+  val lines = source.getLines().toList
+  source.close()
+
+  val stringses = lines.map(line => line.split(" {3}"))
+  val firstList = stringses.map(_(0)).map(_.toInt).sorted(Ordering[Int])
+  val secondList = stringses.map(_(1)).map(_.toInt).sorted(Ordering[Int])
+
+  val sizeDiff = firstList.zip(secondList).map((a, b) => (a - b).abs).sum
+
+  println(sizeDiff)
+
+@main def partTwo(args: String*): Unit =
+  val source = Source.fromResource("dayone/input.txt")
+  val lines = source.getLines().toList
+  source.close()
+
+  val stringses = lines.map(line => line.split(" {3}"))
+  val firstList = stringses.map(_(0)).map(_.toInt)
+  val secondList = stringses.map(_(1)).map(_.toInt)
+
+  val freqMap = secondList.foldLeft(Map.empty[Int, Int]) { (acc, value) =>
+    val runningTotal = acc.getOrElse(value, 0)
+    acc + (value -> (runningTotal + 1))
+  }
+
+  val simScores = firstList.map{ value =>
+    freqMap.getOrElse(value, 0) * value
+  }
+
+  println(simScores.sum)
