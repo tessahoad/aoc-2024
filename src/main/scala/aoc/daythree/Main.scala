82e0e7cdf04ea84cfbc5549380c31617c428ad0d
Tessa Hoad <contact@tessahoad.co.uk>
2024-12-03 13:25:42 +0000

    Day Three part two

--- src/main/scala/aoc/daythree/Main.scala
+++ src/main/scala/aoc/daythree/Main.scala
@@ -20,4 +20,24 @@ object Main extends Utils:
 
     print(products.sum)
 
+  @main def partTwo(args: String*): Unit =
+    val rawCode = readResource("input.txt")
+
+    val pattern = """mul\(\d+,\d+\)|do\(\)|don't\(\)"""
+    val regex = new Regex(pattern)
+    val matches = regex.findAllMatchIn(rawCode).toSeq
+
+    val (products, enabled) = matches.foldLeft((List.empty[Int], true)){ case ((products, isEnabled), m) =>
+      m.toString() match
+        case "do()" => (products, true)
+        case "don't()" => (products, false)
+        case matched if isEnabled =>
+          val numbers = new Regex("""\d+""").findAllMatchIn(matched)
+          val product = numbers.map(num => num.group(0).toInt).product
+          (product :: products, isEnabled)
+        case _ => (products, isEnabled)
+    }
+
+    print(products.sum)
+
 
