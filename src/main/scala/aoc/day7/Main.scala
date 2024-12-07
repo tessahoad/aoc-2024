f9b253105d575126bd8e8243cba1ce39a106f7b2
Tessa Hoad <contact@tessahoad.co.uk>
2024-12-07 14:00:03 +0000

    Day 7 part 2

--- src/main/scala/aoc/day7/Main.scala
+++ src/main/scala/aoc/day7/Main.scala
@@ -7,15 +7,20 @@ import scala.util.matching.Regex
 
 object Main extends Utils:
 
-  enum Operator:
-    case Add, Multiply
-
   case class Equation(solution: Long, operands: Seq[Long])
-
 
   def reduceOperands(operands: Seq[Long]): Seq[Long] = {
     operands.tail.foldLeft(Seq(operands.head)) { case (acc, a) =>
        acc.flatMap(value => Seq(value + a, value * a))
+    }.distinct
+  }
+
+  def reduceOperands2(operands: Seq[Long]): Seq[Long] = {
+    operands.tail.foldLeft(Seq(operands.head)) { case (acc, a) =>
+      acc.flatMap{value =>
+        val concat = value.toString + a.toString
+        Seq(value + a, value * a, concat.toLong)
+      }
     }.distinct
   }
 
@@ -29,3 +34,12 @@ object Main extends Utils:
     val validEquations = parsedEquations.filter(equation => reduceOperands(equation.operands).contains(equation.solution))
     print(validEquations.map(_.solution).sum)
 
+  @main def partTwo(args: String*): Unit =
+    val rawEquations = readResourceLines("input.txt")
+    val parsedEquations = rawEquations
+      .map(raw => raw.split(":"))
+      .map(equation => (equation.head, equation.last))
+      .map((solutionString, operandsString) => Equation(solutionString.toLong, operandsString.trim.split(" ").map(_.toLong)))
+
+    val validEquations = parsedEquations.filter(equation => reduceOperands2(equation.operands).contains(equation.solution))
+    print(validEquations.map(_.solution).sum)
