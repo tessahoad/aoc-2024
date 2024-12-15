7dd74276b90451bf6b4e2ff8e3b9bd5d946327db
Tessa Hoad <contact@tessahoad.co.uk>
2024-12-15 21:01:20 +0000

    Day 9 part two ugly

--- src/main/scala/aoc/day9/Main.scala
+++ src/main/scala/aoc/day9/Main.scala
@@ -8,7 +8,7 @@ import scala.util.matching.Regex
 object Main extends Utils:
 
   extension[T] (seq: Seq[T])
-    def swapAt(fromIndex: Int, toIndex: Int): Seq[T] =
+    private def swapAt(fromIndex: Int, toIndex: Int): Seq[T] =
       if fromIndex == toIndex then seq
       else
         val (minIndex, maxIndex) = if fromIndex < toIndex then (fromIndex, toIndex) else (toIndex, fromIndex)
@@ -28,6 +28,31 @@ object Main extends Utils:
       val swappedBlocks = blocks.swapAt(freeSpaceIndex, blockIndex)
       moveToFreeSpace(swappedBlocks)
     }
+
+
+  @tailrec
+  private def moveToFreeBlocks(blocks: Seq[String], id: String): Seq[String] =
+    val startOfBlockIndex = blocks.indexOf(id)
+    val endOfBlockIndex = blocks.lastIndexOf(id)
+    val spaceRequired = endOfBlockIndex - startOfBlockIndex + 1
+
+    val startOfBigEnoughEmptyBlock = blocks.indexOfSlice(Seq.fill(spaceRequired)("."))
+
+    val updatedBlocks = startOfBigEnoughEmptyBlock match
+      case -1 => blocks
+      case _ =>
+        if startOfBlockIndex < startOfBigEnoughEmptyBlock then
+          blocks
+        else
+         val x = (startOfBlockIndex to endOfBlockIndex).foldLeft(blocks)((acc, index) =>
+            acc.swapAt(index, startOfBigEnoughEmptyBlock + index - startOfBlockIndex))
+         x
+
+    if id.toInt % 100 == 0 then println(s"Moving block $id")
+    if id != "0" then
+      moveToFreeBlocks(updatedBlocks, (id.toLong - 1).toString)
+    else
+      updatedBlocks
 
   private def checksum(blocks: Seq[String]): Long =
     val indexedBlock = blocks.zipWithIndex
@@ -52,6 +77,27 @@ object Main extends Utils:
           fileBlock ++ spaceBlock
     )
     val shuffled = moveToFreeSpace(rearrangedBlocks)
+    println(shuffled)
+    println(checksum(shuffled))
+
+  @main def partTwo(args: String*): Unit =
+    val rawDiskMap = readResource("input.txt")
+    val filesAndFreeSpace = rawDiskMap.split("").map(_.toInt).sliding(2, 2).zipWithIndex.toSeq
+    val rearrangedBlocks = filesAndFreeSpace.flatMap((fileAndFreeSpace, index) =>
+      if fileAndFreeSpace.length == 1 then
+        val numberFiles = fileAndFreeSpace.head
+        val fileBlock = List.fill(numberFiles)(index.toString)
+        fileBlock
+      else
+        val numberFiles = fileAndFreeSpace.head
+        val freeSpace = fileAndFreeSpace.last
+        val fileBlock = List.fill(numberFiles)(index.toString)
+        val spaceBlock = List.fill(freeSpace)(".")
+        fileBlock ++ spaceBlock
+    )
+    val endOfBlockIndex = rearrangedBlocks.lastIndexWhere(_ != ".")
+    val id = rearrangedBlocks(endOfBlockIndex)
+    val shuffled = moveToFreeBlocks(rearrangedBlocks, id)
     println(shuffled)
     println(checksum(shuffled))
 
