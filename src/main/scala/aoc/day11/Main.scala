3f4204bab6185b5f848f2e5b9a3353aa979c5c14
Tessa Hoad <contact@tessahoad.co.uk>
2024-12-16 21:30:47 +0000

    Day 11 part two

--- src/main/scala/aoc/day11/Main.scala
+++ src/main/scala/aoc/day11/Main.scala
@@ -31,3 +31,30 @@ object Main extends Utils:
     )
     println(manyBlinks.size)
 
+  extension (m: mutable.Map[String, Long])
+    def updateOrReplace(key: String, value: Long): Unit =
+      m.get(key) match
+        case Some(existing) => m(key) = existing + value
+        case None => m(key) = value
+
+  @main def partTwo(args: String*): Unit =
+    val unparsedStones = readResource("input.txt")
+    val stones = unparsedStones.split(" ").toSeq
+    val stoneMap: mutable.Map[String, Long] = mutable.Map()
+    stones.foreach(stoneMap.updateOrReplace(_, 1))
+    val finalMap = (1 to 75).foldLeft(stoneMap)((acc, index) =>
+        val newMap: mutable.Map[String, Long] = mutable.Map()
+        acc.keys.foreach { key =>
+          val count = acc.getOrElse(key , 0L)
+          key match
+            case "0" => newMap.updateOrReplace("1", count)
+            case stone if stone.length % 2 == 0 =>
+              val (leftStone, rightStone) = stone.splitAt(stone.length / 2)
+              newMap.updateOrReplace(dropLeadingZeroes(leftStone), count)
+              newMap.updateOrReplace(dropLeadingZeroes(rightStone), count)
+            case stone => newMap.updateOrReplace((stone.toLong * 2024).toString, count)
+        }
+        println(s"Blinked $index times")
+        newMap
+    )
+    println(finalMap.values.sum)
