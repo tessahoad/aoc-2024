ae0383fa9a7273c481af481a275617b09d80c8e7
Tessa Hoad <contact@tessahoad.co.uk>
2024-12-24 22:44:34 +0000

    Day 13 Part Two

--- src/main/scala/aoc/day13/Main.scala
+++ src/main/scala/aoc/day13/Main.scala
@@ -3,23 +3,18 @@ package aoc.day13
 import aoc.common.Utils
 import aoc.day8.Main.readResourceLines
 
-import scala.collection.mutable
-import breeze.linalg._
-
 object Main extends Utils:
 
-  def solveSimultaneousEquations(a1: Double, b1: Double, c1: Double, a2: Double, b2: Double, c2: Double): (Double, Double) = {
-    val A = DenseMatrix((a1, b1), (a2, b2))
-    val C = DenseVector(c1, c2)
-    val X = inv(A) * C
-    val scale = math.pow(10, 6)
-    (math.round(X(0) * scale) / scale, math.round(X(1) * scale) / scale)
+  def numberTokens(a1: Long, b1: Long, c1: Long, a2: Long, b2: Long, c2: Long): Long = {
+    val a = (c1 * b2 - c2 * b1) / (a1 * b2 - a2 * b1)
+    val b = (c1 - a1 * a) / b1
+    if (a * a1 + b * b1 == c1 && a * a2 + b * b2 == c2) 3 * a + b else 0
   }
 
-  case class Button(xTransformer: Double, yTransformer: Double)
-  case class Prize(xValue: Double, yValue: Double)
+  case class Button(xTransformer: Long, yTransformer: Long)
+  case class Prize(xValue: Long, yValue: Long)
 
-  def main(args: Array[String]): Unit = {
+  @main def partOne(args: String*): Unit = {
     val rawConfigurations = readResourceLines("input.txt")
     val configs = rawConfigurations
       .filterNot(_.isEmpty)
@@ -27,30 +22,60 @@ object Main extends Utils:
       .map(config =>
         val aInstructions: Array[String] = config.head.split(":").last.split(",")
         val buttonA = Button(
-          aInstructions.head.split("\\+").last.toDouble,
-          aInstructions.last.split("\\+").last.toDouble
+          aInstructions.head.split("\\+").last.toLong,
+          aInstructions.last.split("\\+").last.toLong
         )
         val bInstructions = config(1).split(":").last.split(",")
         val buttonB = Button(
-          bInstructions.head.split("\\+").last.toDouble,
-          bInstructions.last.split("\\+").last.toDouble
+          bInstructions.head.split("\\+").last.toLong,
+          bInstructions.last.split("\\+").last.toLong
         )
 
         val prizeInstructions = config.last.split(":").last.split(",")
         val prize = Prize(
-          prizeInstructions.head.split("=").last.toDouble,
-          prizeInstructions.last.split("=").last.toDouble
+          prizeInstructions.head.split("=").last.toLong,
+          prizeInstructions.last.split("=").last.toLong
         )
 
         (buttonA, buttonB, prize)
       )
       .toSeq
-    val answers = configs.map(config =>
-      solveSimultaneousEquations(config._1.xTransformer, config._2.xTransformer, config._3.xValue, config._1.yTransformer, config._2.yTransformer, config._3.yValue)
-    ).filter(answer => answer._1.isWhole && answer._2.isWhole)
-
-    val tokens = answers.map(answer => 3*answer._1 + answer._2)
+    val tokens = configs.map(config =>
+      numberTokens(config._1.xTransformer, config._2.xTransformer, config._3.xValue, config._1.yTransformer, config._2.yTransformer, config._3.yValue)
+    )
 
     println(tokens.sum)
   }
+
+  @main def partTwo(args: String*): Unit =
+    val rawConfigurations = readResourceLines("input.txt")
+    val configs = rawConfigurations
+      .filterNot(_.isEmpty)
+      .sliding(3, 3)
+      .map(config =>
+        val aInstructions: Array[String] = config.head.split(":").last.split(",")
+        val buttonA = Button(
+          aInstructions.head.split("\\+").last.toLong,
+          aInstructions.last.split("\\+").last.toLong
+        )
+        val bInstructions = config(1).split(":").last.split(",")
+        val buttonB = Button(
+          bInstructions.head.split("\\+").last.toLong,
+          bInstructions.last.split("\\+").last.toLong
+        )
+
+        val prizeInstructions = config.last.split(":").last.split(",")
+        val prize = Prize(
+          prizeInstructions.head.split("=").last.toLong + 10000000000000L,
+          prizeInstructions.last.split("=").last.toLong + 10000000000000L
+        )
+
+        (buttonA, buttonB, prize)
+      )
+      .toSeq
+    val tokens = configs.map(config =>
+      numberTokens(config._1.xTransformer, config._2.xTransformer, config._3.xValue, config._1.yTransformer, config._2.yTransformer, config._3.yValue)
+    )
+
+    println(tokens.sum)
 
